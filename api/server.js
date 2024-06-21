51c6b4014a74f595b4f425a1bf9bfed983fba6d0
Sonia <soniaveit.moron@gmail.com>
2024-06-21 03:29:59 -0300

    Modificados los archivos vercel.json y server.js para que utilice el index.htm dentro de la carpeta public

--- api/server.js
+++ api/server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Ruta de ejemplo
-app.get('/api', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
+app.get('/api/db', (req, res) => {
+    const dbPath = path.join(__dirname, 'db.json');
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        res.status(500).send('Error leyendo la base de datos');
+      } else {
+        res.json(JSON.parse(data));
+      }
+    });
+  });
 
 // Manejo de rutas inexistentes
 app.use((req, res, next) => {
   res.status(404).send('Página no encontrada');
 });
 
+// Inicia el servidor
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+  });
+  
 // Exporta la aplicación
 module.exports = app;
